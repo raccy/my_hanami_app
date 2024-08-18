10260afb1a6633dcacbec709ce94ceb455c497ee
raccy <raccy@users.noreply.github.com>
2024-08-18 20:55:46 +0900

    Adding our first functionality

---
+++ app/actions/home/show.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+module MyHanamiApp
+  module Actions
+    module Home
+      class Show < MyHanamiApp::Action
+        def handle(request, response)
+        end
+      end
+    end
+  end
+end
--- spec/requests/root_spec.rb
+++ spec/requests/root_spec.rb
@@ -1,11 +1,10 @@
 # frozen_string_literal: true
 
 RSpec.describe "Root", type: :request do
-  it "is not found" do
+  it "is successful" do
     get "/"
 
-    # Generate new action via:
-    #   `bundle exec hanami generate action home.index --url=/`
-    expect(last_response.status).to be(404)
+    expect(last_response.body).to include "Welcome to MyHanamiApp"
+    expect(last_response).to be_successful
   end
 end
