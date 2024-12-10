e6e356c31cbdaa4c606f174f6a70859e4a18b52a
IGARASHI Makoto <igarashi@kyokyo-u.ac.jp>
2024-12-10 16:18:18 +0900

    API Parameter validation

--- slices/api/actions/books/index.rb
+++ slices/api/actions/books/index.rb
@@ -6,8 +6,19 @@ module API
       class Index < API::Action
         include Deps["repos.book_repo"]
 
+        params do
+          optional(:page).value(:integer, gt?: 0)
+          optional(:per_page).value(:integer, gt?: 0, lteq?: 100)
+        end
+
         def handle(request, response)
-          books = book_repo.all_by_title
+          halt 422, {errors: request.params.errors}.to_json unless request.params.valid?
+
+
+          books = book_repo.all_by_title(
+            page: request.params[:page] || 1,
+            per_page: request.params[:per_page] || 5
+          )
 
           response.format = :json
           response.body = books.map(&:to_h).to_json
